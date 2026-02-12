cffe1ab0adf1d94cd94055c4aafdb37b7bfa22f6
ozyilmazsamet9-eng <ozyilmazsamet9@gmail.com>
2026-02-12 05:16:12 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
  * Supabase ayarları. Tüm cihazlarda galeri için supabase-config.example.js
  * içindeki açıklamaya göre doldur. Doldurmazsan yüklemeler sadece bu cihazda (IndexedDB) kalır.
  */
-window.supabaseUrl = sb_publishable_quyszGn4xWABUPoVTLh5zw_jwaJlZTJ
+window.supabaseUrl = https://xzjaylnnbsxcxuqncner.supabase.co
 window.supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6amF5bG5uYnN4Y3h1cW5jbmVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4NTU2NTMsImV4cCI6MjA4NjQzMTY1M30.mR8xfV3PUtdFecpqI-CneenSPXjM8GQJ2uKDcGahc0o
